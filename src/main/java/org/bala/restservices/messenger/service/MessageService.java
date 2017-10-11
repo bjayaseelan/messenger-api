89af68f8613685b6bc828603ad2981ddf4a3763a
Bala Jayaseelan <chatto.bala@gmail.com>
2017-10-11 16:41:11 -0700

    First commit

---
+++ src/main/java/org/bala/restservices/messenger/service/MessageService.java
@@ -0,0 +1,68 @@
+package org.bala.restservices.messenger.service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+import org.bala.restservices.messenger.database.DatabaseClass;
+import org.bala.restservices.messenger.model.Message;
+
+public class MessageService {
+	
+	private static Map<Long, Message> messages = DatabaseClass.getMessages();
+
+	public MessageService(){
+		messages.put(1L, new Message(1L, "Hello World", "Bala"));
+		messages.put(2L, new Message(2L, "Hello Jersey", "Bala"));
+	}
+	
+	
+	public  List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public  List<Message> getMessagesForYear(int year) {
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for(Message message : messages.values()){
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year){
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	
+	public  List<Message> getAllMessagesPaginated(int start, int size) {
+		ArrayList<Message> list = new ArrayList<Message>(messages.values());
+		if(start + size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start + size);
+	}
+	
+	
+	public Message getMessage(long id){
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message){
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message){
+		if(message.getId() <=0){
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
+	}
+	
+	
+}
